9a703935dad294fb162c254141129afae233861a
5252b <5252bb@daum.net>
2018-05-07 23:22:57 +0900

    05 07 23:22 07일일 작업 마감

--- app/src/main/java/kr/saintdev/safetypin/views/fragments/auth/SelectAuthFragment.java
+++ app/src/main/java/kr/saintdev/safetypin/views/fragments/auth/SelectAuthFragment.java
@@ -6,8 +6,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import kr.saintdev.safetypin.R;
+import kr.saintdev.safetypin.views.activitys.AuthmeActivity;
 import kr.saintdev.safetypin.views.fragments.SuperFragment;
 
 /**
@@ -15,6 +17,11 @@ import kr.saintdev.safetypin.views.fragments.SuperFragment;
  */
 
 public class SelectAuthFragment extends SuperFragment {
+    AuthmeActivity control = null;
+
+    Button gotoLoginPage = null;
+    Button gotoSignupPage = null;
+
     public SelectAuthFragment() {
     }
 
@@ -22,6 +29,28 @@ public class SelectAuthFragment extends SuperFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragmn_auth_select, container, false);
+        this.control = (AuthmeActivity) getActivity();
+
+        this.gotoLoginPage = v.findViewById(R.id.auth_select_login);
+        this.gotoSignupPage = v.findViewById(R.id.auth_select_signup);
+
+        OnButtonClickHandler handler = new OnButtonClickHandler();
+        this.gotoSignupPage.setOnClickListener(handler);
+        this.gotoLoginPage.setOnClickListener(handler);
+
         return v;
+    }
+
+    class OnButtonClickHandler implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.auth_select_login:
+                    control.switchFragment(new LoginFragment());
+                    break;
+                case R.id.auth_select_signup:
+                    break;
+            }
+        }
     }
 }
